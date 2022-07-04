7a583272e03ae7fea36f8e46ffff4dd1bd55116f
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2022-07-04 07:54:57 +0700

    upload submisi

--- lib/DetailFood.dart
+++ lib/DetailFood.dart
@@ -4,6 +4,7 @@ import 'package:popular_food/model/Food.dart';
 
 class DetailFood extends StatefulWidget {
   final Food food;
+
   const DetailFood({Key? key, required this.food}) : super(key: key);
 
   @override
@@ -11,6 +12,30 @@ class DetailFood extends StatefulWidget {
 }
 
 class _DetailFoodState extends State<DetailFood> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 992) {
+          return DetailWebPage(food: widget.food);
+        } else {
+          return DetailMobilePage(food: widget.food);
+        }
+      },
+    );
+  }
+}
+
+class DetailMobilePage extends StatefulWidget {
+  final Food food;
+
+  const DetailMobilePage({Key? key, required this.food}) : super(key: key);
+
+  @override
+  State<DetailMobilePage> createState() => _DetailMobilePageState();
+}
+
+class _DetailMobilePageState extends State<DetailMobilePage> {
   String size = 'S';
   int quantity = 1;
   bool isFavorite = false;
@@ -98,7 +123,7 @@ class _DetailFoodState extends State<DetailFood> {
                             children: [
                               ElevatedButton(
                                 onPressed: () {
-                                  setState((){
+                                  setState(() {
                                     size = 'S';
                                   });
                                 },
@@ -123,7 +148,7 @@ class _DetailFoodState extends State<DetailFood> {
                               SizedBox(width: 15),
                               ElevatedButton(
                                 onPressed: () {
-                                  setState((){
+                                  setState(() {
                                     size = 'M';
                                   });
                                 },
@@ -147,7 +172,7 @@ class _DetailFoodState extends State<DetailFood> {
                               SizedBox(width: 15),
                               ElevatedButton(
                                 onPressed: () {
-                                  setState((){
+                                  setState(() {
                                     size = 'L';
                                   });
                                 },
@@ -191,8 +216,8 @@ class _DetailFoodState extends State<DetailFood> {
                             children: [
                               ElevatedButton(
                                 onPressed: () {
-                                  if(quantity > 1){
-                                    setState((){
+                                  if (quantity > 1) {
+                                    setState(() {
                                       quantity -= 1;
                                     });
                                   }
@@ -222,9 +247,9 @@ class _DetailFoodState extends State<DetailFood> {
                               SizedBox(width: 10),
                               ElevatedButton(
                                 onPressed: () {
-                                    setState((){
-                                      quantity += 1;
-                                    });
+                                  setState(() {
+                                    quantity += 1;
+                                  });
                                 },
                                 child: Text(
                                   "+",
@@ -277,7 +302,7 @@ class _DetailFoodState extends State<DetailFood> {
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      setState((){
+                      setState(() {
                         isFavorite = !isFavorite;
                       });
                     },
@@ -309,6 +334,313 @@ class _DetailFoodState extends State<DetailFood> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailWebPage extends StatefulWidget {
+  final Food food;
+
+  const DetailWebPage({Key? key, required this.food}) : super(key: key);
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  String size = 'S';
+  int quantity = 1;
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'IDR ',
+      decimalDigits: 0,
+    );
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 20,
+                  left: 10,
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back_ios_new),
+                  alignment: Alignment.centerLeft,
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset(
+                        'assets/images/${widget.food.imageAsset}',
+                        width: 300.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.food.name,
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          widget.food.description,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Color(0xffA4A4A4),
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Size",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      size = 'S';
+                                    });
+                                  },
+                                  child: Text(
+                                    "S",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w600,
+                                      color: size == 'S'
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder(),
+                                    padding: EdgeInsets.all(12),
+                                    primary: size == 'S'
+                                        ? Colors.green
+                                        : Colors.grey[300],
+                                  ),
+                                ),
+                                SizedBox(width: 15),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      size = 'M';
+                                    });
+                                  },
+                                  child: Text(
+                                    "M",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w600,
+                                      color: size == 'M'
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      padding: EdgeInsets.all(12),
+                                      primary: size == 'M'
+                                          ? Colors.green
+                                          : Colors.grey[300]),
+                                ),
+                                SizedBox(width: 15),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      size = 'L';
+                                    });
+                                  },
+                                  child: Text(
+                                    "L",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w600,
+                                      color: size == 'L'
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      padding: EdgeInsets.all(12),
+                                      primary: size == 'L'
+                                          ? Colors.green
+                                          : Colors.grey[300]),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Quantity",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    if (quantity > 1) {
+                                      setState(() {
+                                        quantity -= 1;
+                                      });
+                                    }
+                                  },
+                                  child: Text(
+                                    "-",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder(),
+                                    padding: EdgeInsets.all(3),
+                                    primary: Colors.grey[300],
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  quantity.toString(),
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      quantity += 1;
+                                    });
+                                  },
+                                  child: Text(
+                                    "+",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      padding: EdgeInsets.all(3),
+                                      primary: Colors.green),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 40),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Price",
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w600,
+                                        color: Color(0xffA4A4A4),
+                                      ),
+                                    ),
+                                    Text(
+                                      currencyFormatter
+                                          .format(widget.food.price),
+                                      style: TextStyle(
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isFavorite = !isFavorite;
+                                  });
+                                },
+                                child: Icon(
+                                  isFavorite
+                                      ? Icons.favorite
+                                      : Icons.favorite_border,
+                                  size: 30.0,
+                                  color: isFavorite ? Colors.red : Colors.black,
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(14),
+                                  primary: Colors.grey[300],
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.shopping_basket_outlined,
+                                  size: 30.0,
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(14),
+                                  primary: Colors.green,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -11,26 +11,31 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Popular Food'),
+        title: Text('Central Pizza'),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                margin: EdgeInsets.all(20.0),
-                child: Text(
-                  "Popular Food",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
+          child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(20.0),
+                  child: Text(
+                    "Popular Pizza",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              PopularFoodList(),
-            ],
-          ),
+                constraints.maxWidth <= 992
+                    ? PopularFoodList()
+                    : PopularFoodGrid(),
+              ],
+            );
+          }),
         ),
       ),
     );
@@ -122,6 +127,104 @@ class PopularFoodList extends StatelessWidget {
       shrinkWrap: true,
       physics: ScrollPhysics(),
       itemCount: popularFoodList.length,
+    );
+  }
+}
+
+class PopularFoodGrid extends StatelessWidget {
+  const PopularFoodGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight) / 4;
+    final double itemWidth = size.width / 2;
+    final _scrollController = ScrollController();
+
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'IDR ',
+      decimalDigits: 0,
+    );
+
+    return Scrollbar(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: (itemWidth / itemHeight),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+          controller: _scrollController,
+          children: popularFoodList.map((food) {
+            return InkWell(
+              borderRadius: BorderRadius.circular(12.0),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailFood(food: food);
+                }));
+              },
+              child: Container(
+                height: 140,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 25.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffF6F7FB),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/images/${food.imageAsset}',
+                      width: 68,
+                      height: 70,
+                    ),
+                    SizedBox(width: 18.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            food.name,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff111111),
+                            ),
+                          ),
+                          Text(
+                            food.description,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: TextStyle(
+                              color: Color(0xffA4A4A4),
+                            ),
+                          ),
+                          Text(
+                            currencyFormatter.format(food.price),
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xff111111),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
